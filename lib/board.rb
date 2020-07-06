a0ff16dcab3ba13784b7ba75cf9aa7c57c9e50ca
Felipe Dacal Fragoso <fefragoso@gmail.com>
2020-07-06 14:33:15 +0200

    print board

--- lib/board.rb
+++ lib/board.rb
@@ -1,40 +1,50 @@
-board = (1..9).to_a
+class Board
+  attr_accessor :board
+  
+  def initialize
+    @board = (1..9).to_a
+  end
 
-# DISPLAY THE BOARD
-def display_board(board)
-    puts " #{board[0]} | #{board[1]} | #{board[2]} "
-    puts '-----------'
-    puts " #{board[3]} | #{board[4]} | #{board[5]} "
-    puts '-----------'
-    puts " #{board[6]} | #{board[7]} | #{board[8]} "
-end
+  def get_board
+    return @board
+  end
+  # DISPLAY THE BOARD
+  def display_board(board)
+    return board
+      puts " #{board[0]} | #{board[1]} | #{board[2]} "
+      puts '-----------'
+      puts " #{board[3]} | #{board[4]} | #{board[5]} "
+      puts '-----------'
+      puts " #{board[6]} | #{board[7]} | #{board[8]} "
+  end
 
-# VALIDATE THE MOVE
-def valid_move?(board, index)
+  # VALIDATE THE MOVE
+  def valid_move?(board, index)
     # THE PLAYER INPUT IS BETWEEN 0 AND 8 AND THE POSITION IS NOT TAKEN
     return true if index.between?(0, 8) && !position_taken?(board, index)
-end
+  end
 
-# USER MOVEMENT - X or O to a certain position
-def move(board, index, player)
+  # USER MOVEMENT - X or O to a certain position
+  def move(board, index, player)
     board[index] = player
   end
-  
-# THE POSITION IS ALREADY TAKEN
-def position_taken?(board, index)
-  if ((board[index]).is_a? Integer) || board[index].nil?
-    false
-  else
-    true
+    
+  # THE POSITION IS ALREADY TAKEN
+  def position_taken?(board, index)
+    if ((board[index]).is_a? Integer) || board[index].nil?
+      false
+    else
+      true
+    end
   end
-end
 
-# USER INPUT TO THE ARRAY POSITION
-def input_to_index(user_input)
-  user_input.to_i - 1
-end
+  # USER INPUT TO THE ARRAY POSITION
+  def input_to_index(user_input)
+    user_input.to_i - 1
+  end
 
-# Draw - board is full and without a winner
-def full?(board)
-    board.all? { |i| i.to_s.include?('X') || i.to_s.include?('O') }
+  # Draw - board is full and without a winner
+  def full?(board)
+      board.all? { |i| i.to_s.include?('X') || i.to_s.include?('O') }
+  end
 end
