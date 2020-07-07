46e9e93589e36dd24d1509c3a4477c01817ffa81
Felipe Dacal Fragoso <fefragoso@gmail.com>
2020-07-07 15:09:22 +0200

    Fix rubocop errors

--- bin/main.rb
+++ bin/main.rb
@@ -1,7 +1,7 @@
 # A file which has a shebang line as its first line is
 # granted execute permission.
 
-#!/usr/bin/env ruby
+# !/usr/bin/env ruby
 require_relative '../lib/game.rb'
 require_relative '../lib/board.rb'
 
@@ -20,7 +20,6 @@ def display_board(board)
 end
 
 # initial instructions for player
-
 display_board(board)
 
 until obj_game.game_over?(board, obj_board)
